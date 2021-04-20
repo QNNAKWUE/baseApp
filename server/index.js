7495ff9a38b5ddeb28902e124571e6360e7c06dd
NonsoAmadi10 <nonsoamadi@aol.com>
2021-04-20 11:46:41 +0100

    feature: user can create an account

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 
 const app = express();
+
+// This is a body parser middleware that allows 
+// your express application to receive json body in the request
+app.use(express.json());
+
+//register routes
+
+app.use(userRoutes);
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to my TaskList API' }));
 
 const port = process.env.PORT || 6010;
 
 app.listen(port, () => console.log('Listening on port 6010'));
+
+export default app;
---
+++ server/validator/index.js
@@ -0,0 +1,33 @@
+class Validate {
+  static isEmail(email) {
+    const re = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}/ig;
+    return re.test(email);
+  }
+
+  static isValidParamsLength(param, length) {
+    return param.length < length;
+  }
+
+  static checkEmpty(input) {
+    const re = /^$/;
+    const testBody = re.test(input);
+    return testBody;
+  }
+
+  static containsNumber(name) {
+    const re = /[0-9]/g;
+    const testName = re.test(name);
+    return testName;
+  }
+
+  static isMatchingPassword(password, confirmPassword) {
+    return password === confirmPassword;
+  }
+
+  static itsaNumber(item) {
+    const re = /^[-+]?\d*$/;
+    return re.test(item);
+  }
+}
+
+export default Validate;
