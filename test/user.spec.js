8a0dda491c6b1e1417753bb19f1bb6661571e9f0
Queen Nnakwue <qnnakwue@gmail.com>
2021-05-05 04:07:34 +0100

    fixing bugs

--- test/user.spec.js
+++ test/user.spec.js
@@ -57,7 +57,7 @@ describe('User should be able to login', () => {
       .send({
         email: 'nonsoamadi@aol.com',
         password: 'password',
-        secret: '${process.env.TOKEN_SECRET}'
+        adminSecret: 'johnny'
       })
       .end((err, res) => {
         expect(res.status).to.equal(200);
