f9a42f66cc025f0d06561c7c66ac6043f7dc8038
Queen Nnakwue <qnnakwue@gmail.com>
2021-05-06 02:35:06 +0100

    creating the get/read feature

--- server/routes/ListRoutes.js
+++ server/routes/ListRoutes.js
@@ -6,6 +6,7 @@ import ListController from '../controllers/ListController';
 const router = Router();
 
 router.post('/createList', Auth, Sanitize.listSanitizer, ListController.createListController);
-router.get('/getLists', Auth, ListController.getAllList)
+router.get('/getLists', Auth, ListController.getAllList);
+router.get('getList/:id', ListController.getListById);
 
 export default router;
