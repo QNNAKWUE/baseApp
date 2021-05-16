d7fa7f0e62ac041e9f27090992fa4404dc5101f5
Queen Nnakwue <qnnakwue@gmail.com>
2021-05-16 22:23:10 +0100

    added some features

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,32 @@
-import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
 
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    min: 5,
+    max: 25,
+    required: [true, "can't be blank"],
+},
 
-const User = [{
-  id: 1,
-  fullname: 'Nonso Amadi',
-  email: 'nonsoamadi@aol.com',
-  password: bcrypt.hashSync('password', 10),
-}];
+email: {
+    type: String,
+    min: 5,
+    max: 255,
+    required: [true, "can't be blank"],
+    unique: true,
+    lowercase: true,
+},
+
+password: {
+    type: String,
+    required: [true, "can't be blank"],
+    min: 5,
+    max: 255
+},
+  
+});
+
+const User = mongoose.model('User', userSchema);
+const user = new User({});
 
 export default User;
---
+++ test/index.spec.js
@@ -0,0 +1,67 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../server';
+
+chai.use(chaiHttp);
+// before('Populating the DB', (done)=>{
+//   chai.request(app)
+//   .post('/api/v1/auth/signup')
+//   .send({
+//     fullname: 'whitey',
+//     email: 'jwhitey@gmail.com',
+//     password: 'jwhite',
+//     confirmPassword: 'jwhite'
+//   })
+//   done();
+// })
+
+describe('Auth', () => {
+  it('should allow a user to signup', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        fullname: 'Kim Kardashian',
+        email: 'kimK@gmail.com',
+        password: '12345678',
+        confirmPassword: '12345678',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.a('object');
+        expect(res.status).to.equal(201);
+        expect(res.body.data).to.have.all.keys(['id', 'email', 'fullname']);
+        done();
+      });
+  });
+
+  it('should not allow an existing user to signup', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        fullname: 'Nonso Amadi',
+        email: 'nonsoamadi@aol.com',
+        password: 'password',
+        confirmPassword: 'password',
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(409);
+        expect(res.body.message).to.equal('User exists, please login!');
+        done();
+      });
+  });
+});
+
+describe('User should be able to login', () => {
+  it('should login a registered user', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'nonsoamadi@aol.com',
+        password: 'password'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.data).to.have.all.keys(['id', 'token']);
+        done();
+      });
+  });
+});
